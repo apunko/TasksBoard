f02a78aa66ee5e8cd5c1b9e94b904f3f3489a98d
anton <ant.punko@gmail.com>
2015-09-15 23:27:00 +0300

    add AnswerAttempt model

---
+++ db/migrate/20150915202439_create_answer_attempts.rb
@@ -0,0 +1,13 @@
+class CreateAnswerAttempts < ActiveRecord::Migration
+  def change
+    create_table :answer_attempts do |t|
+      t.string :value
+      t.string :string
+      t.boolean :result
+      t.references :task, index: true, foreign_key: true
+      t.references :user, index: true, foreign_key: true
+
+      t.timestamps null: false
+    end
+  end
+end
